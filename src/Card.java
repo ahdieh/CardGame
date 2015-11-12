74f9699a4021589a179cea0c75650b5ce8453f6e
ahdieh <aemadi@my.centennialcollege.ca>
2015-11-11 18:36:34 -0600

    create Card class

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,19 @@
 
 public class Card {
 
+	// PRIVATE VARIABELS +++++++++++++++
+	private final String _face;
+	private final String _suit;
 	
+	// CONSTRUCTOR +++++++++++++++++
+	public Card(String face,String suit){
+		this._face = face;
+		this._suit = suit;
+			
+	}
+	
+	// PUBLIC METHODS +++++++++
+	public String toString(){
+		return String.format(this._face,"of", this._suit);
+	}
 }
